f3ad412a5de63699ffca972151d896526f121e26
lena-almahdi <lenaalmahdi54321@gmail.com>
2021-07-31 11:37:09 +0300

    edit login and register view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,5 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+    <x-auth-card1>
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
@@ -12,45 +7,47 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
+        <h1 class="tilte signin is-3"> Sign In</h1>
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
             <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+            <div class=" logdiv1">
+                <x-label1 for="email" class="loglabel" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="block mt-1 loginput" type="email" name="email" :value="old('email')" required
+                    autofocus />
             </div>
 
             <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+            <div class=" logdiv2">
+                <x-label1 for="password" class="loglabel" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                <x-input id="password" class="block  loginput mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
             <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            <div class="block remeber mt-4">
+                <label for="remember_me">
+                    <input id="remember_me" type="checkbox"
+                        class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 "
+                        name="remember">
+                    <span class="ml-2 ">{{ __('Remember me') }}</span>
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center forgotpassword justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline forgot " href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
 
-                <x-button class="ml-3">
+                <x-button class=" logbutton ">
                     {{ __('Log in') }}
                 </x-button>
             </div>
         </form>
-    </x-auth-card>
+        </x-auth-card>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,43 +1,23 @@
 <x-guest-layout>
     <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
+        <h1 class="tilte signup is-3"> Sign Up</h1>
+        <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data" class="authform ">
             @csrf
 
             <!-- Name -->
             <div>
-                <x-input id="name" class="block mt-1 w-full" type="text" name="full_name" :value="old('full_name')"
+                <x-label for="name" :value="__('Full Name')" />
+                <x-input id="name" class="block  authinput  " type="text" name="full_name" :value="old('full_name')"
                     required autofocus />
             </div><br>
 
-            {{-- photo --}}
-            <div class="flex w-full items-center justify-center bg-grey-lighter">
-                <label
-                    class="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white">
-                    <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path
-                            d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
-                    </svg>
-                    <span class="mt-2 text-base leading-normal">Select a file</span>
-                    <input type='file' class="hidden" name="photo" />
-                </label>
-            </div>
-
-
             {{-- Acadimec Year --}}
-            <div class="field">
-                <label class="label">Academic Year</label>
-                <div class="control">
-                    <div class="select">
-                        <select name="year_id">
+            <div class="field authyear ml-5">
+                <label class="label authtitle ">Academic Year</label>
+                <div class="control ">
+                    <div class="select ">
+                        <select name="year_id" class="authinput">
                             @foreach ($years as $year)
                                 <option value="{{ $year->id }}">{{ $year->name }}</option>
                             @endforeach
@@ -50,55 +30,69 @@
             </div>
 
             {{-- phone lable --}}
-            <div>
-                <x-label for="phone" :value="__('phone')" />
+            <div class="mt-4">
+                <x-label for="phone" :value="__('Phone')" />
 
-                <x-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')" required />
+                <x-input id="phone" class="block   authinput" type="text" name="phone" :value="old('phone')" required />
             </div>
             <!-- Email Address -->
-            <div class="mt-4">
+            <div class="mt-4 authyear">
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                <x-input id="email" class="block  authinput" type="email" name="email" :value="old('email')" required />
+            </div>
+
+            <!-- Password -->
+
+            <div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
+
+                <x-input id="password" class="block  authinput " type="password" name="password" required
+                    autocomplete="new-password" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4 authyear">
+                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-input id="password_confirmation" class="block authinput" type="password" name="password_confirmation"
                     required />
             </div>
-            {{-- role --}}
-            <div class="field">
-                <label class="checkbox">
-                    <input type="hidden" name="role_id" value="0">
-                    <input type="checkbox" name="role_id" value="{{ $role->id }}">
-                    {{ $role->name }}
+            {{-- photo --}}
+            <div class="mt-4 picture">
+                <label class=" flex flex-col items-center authinput  authphoto border border-blue  ">
+                    <svg class="w-10 h-10" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path
+                            d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+                    </svg>
+                    <span class="mt-2  authtitle text-base leading-normal">Upload a Photo</span>
+                    <input type='file' class="hidden" name="photo" />
                 </label>
             </div>
-            <!-- Password -->
-            <div class="field">
-                <div class="mt-4">
-                    <x-label for="password" :value="__('Password')" />
+            {{-- role --}}
+            <div class="field authrole mt-6">
+                <label class="checkbox authtitle ">
+                    <input type="hidden" name="role_id" value="0">
+                    <input type="checkbox" name="role_id" value="{{ $role->id }}">
+                    I am a {{ $role->name }}
+                </label>
+            </div>
 
-                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                        autocomplete="new-password" />
-                </div>
 
-                <!-- Confirm Password -->
-                <div class="mt-4">
-                    <x-label for="password_confirmation" :value="__('Confirm Password')" />
+            <div class="regbutton">
+                <a class="underline alreadyreg authtitle" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+                <x-button class=" reg authtitle">
+                    {{ __('Register') }}
+                </x-button>
 
-                    <x-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                        name="password_confirmation" required />
-                </div>
 
-                <div class="flex items-center justify-end mt-4">
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                        {{ __('Already registered?') }}
-                    </a>
-
-                    <x-button class="ml-4">
-                        {{ __('Register') }}
-                    </x-button>
-                </div>
+            </div>
 
             </div>
         </form>
+
     </x-auth-card>
 </x-guest-layout>
 
